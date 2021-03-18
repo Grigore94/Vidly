37cba441cf227bcc85efddf5360a08fe25c72785
Grigore <grigore.mereacre95@gmail.com>
2021-03-18 16:02:48 -0500

    changed state via handlelike eventhandler for  ui

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -11,50 +11,56 @@ class Movies extends Component {
     //modern js if key and value are the same we remove repetition movies: movie => movies
     this.setState({ movies });
   };
-  handleLike = () => {
-      
-  }
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movie[index].liked;
+    this.setState({ movies });
+  };
   render() {
-      // obj destructuring setin this.satte.movie as count variable
-      const { length : count } = this.state.movies;
-      if(count === 0) return <p>No movies scheduled for today</p>
+    // obj destructuring setin this.satte.movie as count variable
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>No movies scheduled for today</p>;
     return (
-        <div>
-        <p>We are showing { count } movies today</p>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th />
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                  <Like liked={movie.liked} onClick={this.handleLike}/>
-                  </td>
-              <td>
-                
-                <button
-                  onClick={() => this.handleDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+      <div>
+        <p>We are showing {count} movies today</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
