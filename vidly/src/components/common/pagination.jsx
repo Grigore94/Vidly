c72f38e753cb6540bf7065e05c9e2bbca94ec2d5
Grigore <grigore.mereacre95@gmail.com>
2021-03-18 17:36:56 -0500

    conditional to not return only one page if that is the case and if it is a foating number using math.ceil method to round it to an integer

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
+//called _ from a popular js library called underscore
+import _ from "lodash";
 
 const Pagination = (props) => {
-    //maping eatch page number
-    const {itemsCount, pageSize } = props;
-    const pageCount = itemsCount / pageSize;
-    return ( <nav>
-        <ul>
-            <li className="page-item"><a className="page-link"></a>1</li>
-        </ul>
-    </nav> );
-}
- 
-export default Paginatinullon;
+  //maping eatch page number
+  const { itemsCount, pageSize } = props;
+  const pageCount = Math.ceil(itemsCount / pageSize);
+  //using loadash to generate an array of numbers
+  //+1 to make sure last page is also imcluded
+  if (pageCount === 1 ) return null;
+  const pages = _.range(1, pageCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={pages} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
