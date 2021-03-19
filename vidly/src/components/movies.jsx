76390f7dba35f1d9ae5710d15eed642fa770d832
Grigore <grigore.mereacre95@gmail.com>
2021-03-18 20:32:31 -0500

    added default props to simplify interface for list group

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -14,7 +14,7 @@ class Movies extends Component {
     genres: [],
   };
   componentDidMount() {
-    this.setState({movies: getMovies(), genres: getGenres()});
+    this.setState({ movies: getMovies(), genres: getGenres() });
   }
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -32,8 +32,8 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
   handleGenreSelect = (genres) => {
-    console.log(genres)
-  }
+    console.log(genres);
+  };
   render() {
     // obj destructuring setin this.satte.movie as count variable
     const { length: count } = this.state.movies;
@@ -44,56 +44,57 @@ class Movies extends Component {
     return (
       <div className="row">
         <div className="col-2">
-          <ListGroup item={this.state.genres} onitemSelect={this.handeGenreSelect}/>
+          <ListGroup
+            item={this.state.genres}
+            onitemSelect={this.handeGenreSelect}
+          />
         </div>
         <div className="col">
-       
-        <p>We are showing {count} movies today</p>
-        
-        <table className="table">
-        
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}
-                  />
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
+          <p>We are showing {count} movies today</p>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th />
+                <th />
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
+            </thead>
+
+            <tbody>
+              {movies.map((movie) => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
         </div>
       </div>
     );
