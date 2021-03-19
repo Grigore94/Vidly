7d0bfc85bd4ced22bdd9a5c35bed94f2400781fc
Grigore <grigore.mereacre95@gmail.com>
2021-03-18 21:06:54 -0500

    brought the tables from movie component to a new movietable component to differentiate high level components from low level bringing them pn the same level of abstraction

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
-import Like from "./common/like";
+import MoviesTable from "./moviesTable";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
@@ -14,7 +14,8 @@ class Movies extends Component {
     genres: [],
   };
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres" }, ...getGenres];
+    this.setState({ movies: getMovies(), genres });
   }
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -31,8 +32,9 @@ class Movies extends Component {
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
   };
+  //setting current page to 1 when navigating through other genres
   handleGenreSelect = (genre) => {
-    this.seState({ selectedGenre: genre });
+    this.seState({ selectedGenre: genre, currentPage: 1 });
   };
   render() {
     // obj destructuring setin this.satte.movie as count variable
@@ -44,10 +46,11 @@ class Movies extends Component {
       movies: allMovies,
     } = this.state;
     if (count === 0) return <p>No movies scheduled for today</p>;
-    //ternary operator if selectedgenre is truthy we get all movies and filter them 
-    const filltered = selectedGenre
-      ? allMovies.filter((m) => m.genre_id === selectedGenre._id)
-      : allMovies;
+    //ternary operator if selectedgenre is truthy we get all movies and filter them
+    const filltered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre_id === selectedGenre._id)
+        : allMovies;
     const movies = paginate(filltered, currentPage, pageSize);
     return (
       <div className="row">
@@ -60,44 +63,12 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>We are showing {filltered.length} movies today</p>
+          <MoviesTable
+            movies={movies}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+          />
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => this.handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
           <Pagination
             itemsCount={filltered.length}
             pageSize={pageSize}
