d4c635413bafad95cdb7383a29adc524b3ddb8dd
Grigore <grigore.mereacre95@gmail.com>
2021-03-19 18:55:44 -0500

    syncd the acc obj with login form added handle change method

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,26 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class LoginForm extends Component {
-    handleSubmit = e => {
-        e.preventDefault();
-    }
-    render() { 
-        return ( <div>
-            <h1>Login</h1>
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-grop">
-                    <lable htmlFor="username">Username</lable>
-                    <input id="username" type="text" className="form-control"/>
-                </div>
-                <div className="form-grop">
-                    <lable htmlFor="password">Passord</lable>
-                    <input id="password" type="text" className="form-control"/>
-                </div>
-                <button className="btn btn-prinary">Login</button>
-                <button className="btn btn-prinary">SignUP</button>
-            </form>
-        </div> );
-    }
+  handleSubmit = (e) => {
+    state = {
+      account: { username: "", password: "" },
+    };
+    e.preventDefault();
+  };
+  handleChange = ({currentTarget: input}) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+      const {account} = this.state
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-grop">
+            <lable htmlFor="username">Username</lable>
+            <input
+              autoFocus
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-grop">
+            <lable htmlFor="password">Password</lable>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-prinary">Login</button>
+          <button className="btn btn-prinary">SignUP</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
