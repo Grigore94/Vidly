d7e4faa190c47e2a82eff70a48972bf60f9dfa4d
Grigore <grigore.mereacre95@gmail.com>
2021-04-01 11:42:56 -0500

    renderSelect method added to render a dropdown list with all the good stuff

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -1,58 +1,77 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import {getMovie, saveMovie } from "../services/fakeMovieService";
-import {gotGenres} from "../services/fakeGenreService";
+import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { gotGenres } from "../services/fakeGenreService";
 
 class MovieForm extends Form {
   state = {
     data: {
-      title:"",
+      title: "",
       genreId: "",
-      numberInStock:"",
-      dailyRentalRate: ""
+      numberInStock: "",
+      dailyRentalRate: "",
     },
     genres: [],
-    errors: {}
+    errors: {},
   };
   schema = {
     _id: Joi.string(),
     title: Joi.string().required().label("Title"),
     genreId: Joi.string().required().label("Genre"),
-    numberInStock: Joi.number().required().min(0).max(100).label("Number in stock"),
-    dailyRentalRate: Joi.number().required().min(0).max(10).label("Daily Rental Rate")
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate"),
   };
 
   componentDidMount() {
     const genres = gotgenres();
-    this.setState({genres});
+    this.setState({ genres });
 
     const movieId = this.props.match.params.id;
-    if(movieId === "new") return;
+    if (movieId === "new") return;
 
     const movie = getMovie(movieId);
-    if(!movie) return this.props.history.replace("/not-found");
+    if (!movie) return this.props.history.replace("/not-found");
 
-    this.setState({data: this.mapToViewModel(movie)});
+    this.setState({ data: this.mapToViewModel(movie) });
   }
 
   mapToViewModel(movie) {
-    return{
+    return {
       _id: movie._id,
       title: movie.title,
       genreId: movie.genre._id,
       numberInStock: movie.numberInStock,
-      dailyRentalRate: movie.dailyRentalRate
+      dailyRentalRate: movie.dailyRentalRate,
     };
   }
   doSubmit = () => {
     saveMovie(this.state.data);
     this.props.history.push("/movies");
-  }
+  };
 
-  render() { 
-    return (  );
+  render() {
+    return (
+      <div>
+        <h1>Movie form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderImput("thitle", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderImput("numberInStock", "Number in stock", "number")}
+          {this.renderImput("dailyRentalRate", "Rate")}
+          {this.renderButton("Save")}
+        </form>
+      </div>
+    );
   }
 }
- 
+
 export default MovieForm;
