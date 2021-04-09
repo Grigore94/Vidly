d8f69f16c378d56f9299828c3c14b99fb10f6f44
Grigore <grigore.mereacre95@gmail.com>
2021-04-09 15:21:38 -0500

    handled the errors with a try catch block

--- vidly-frontend/src/components/loginForm.jsx
+++ vidly-frontend/src/components/loginForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import {login } from "../services/authService"
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -13,9 +13,17 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  doSubmit = async() => {
-    const {data} = this.state;
-    await login(data.username, data.password)
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await login(data.username, data.password);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.respose.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
