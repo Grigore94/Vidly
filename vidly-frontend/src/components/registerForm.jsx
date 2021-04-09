c953b9a92aa67db12a6ca3767c121055ad22a59a
Grigore <grigore.mereacre95@gmail.com>
2021-04-09 15:41:20 -0500

    returned custom web token in headers and store it in local storage also regidercting user to home page after registration

--- vidly-frontend/src/components/registerForm.jsx
+++ vidly-frontend/src/components/registerForm.jsx
@@ -19,6 +19,8 @@ class RegisterForm extends Form {
     try {
       const response = await userService.register(this.state.data);
       console.log(response);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -26,7 +28,7 @@ class RegisterForm extends Form {
         this.setState({ errors });
       }
     }
-  }; 
+  };
 
   render() {
     return (
